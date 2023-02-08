9096c56b90e5a8e910a93eb84d84a541ee894a21
Daniel <vernond@tcd.ie>
2023-02-08 23:17:46 +0000

    notes part 3c

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -20,40 +20,77 @@ app.get('/api/notes', (request, response) => {
 })
 
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
-    })
+
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).send({error: 'not found'})
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.put('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        const body = request.body 
-
-        if (body.important === undefined) {
-            return response.status(400).json({ error: 'content missing' })
-        }
-        note.important = body.important
-        response.json(note)
-    })
-})
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-  
+
     if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
+        return response.status(400).json({ error: 'content missing' })
     }
-  
+
     const note = new Note({
-      content: body.content,
-      important: body.important || false,
+        content: body.content,
+        important: body.important || false,
     })
-  
+
     note.save().then(savedNote => {
-      response.json(savedNote)
+        response.json(savedNote)
     })
 })
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
