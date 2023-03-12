3b5622f5246bdc58aec81fb8e488ecd78b317881
Daniel <vernond@tcd.ie>
2023-03-12 18:36:46 +0000

    Ex 4.2 blog refactoring

---
+++ part4/blog/app.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+const blogRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
