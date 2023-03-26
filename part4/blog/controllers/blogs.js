9f3e0c4637ac72a490f89686a70be39eb9141575
Daniel <vernond@tcd.ie>
2023-03-26 20:23:24 +0100

    Up to ex 4.14

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -8,9 +8,36 @@ blogRouter.get('/', async (request, response) => {
 })
 
 blogRouter.post('/', async (request, response) => {
+  if (request.body.title === undefined) {
+    return response.status(400).send("Error! No title.")
+  }
+
+  if (request.body.url === undefined) {
+    return response.status(400).send("Error! No URL.")
+  }
+
   const blog = new Blog(request.body)
+
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
+})
+
+blogRouter.put('/:id', async (request, response, next) => {
+  const {title, author, url, likes } = request.body
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    {title, author, url, likes},
+    { new: true, runValidators: true, context: 'query' }
+  )
+  response.status(204).json(updatedBlog)
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  Blog
+    .findByIdAndDelete(request.params.id)
+    .then(() => response.status(204).end())
+    .catch(() => response.status(404).end())
 })
 
 module.exports = blogRouter
--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -4,6 +4,7 @@ const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const firstBlog = require('../models/blog')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -36,6 +37,13 @@ describe('Get blogs', () => {
       'The Art of Photography'
     )
   })
+
+  test('unique id property of blog posts is named id', async () => {
+    const blogs = await helper.blogsInDb()
+    const firstBlog = blogs[0]
+
+    expect(firstBlog.id).toBeDefined()
+  })
 })
 
 describe('Make new blogs', () => {
@@ -62,7 +70,87 @@ describe('Make new blogs', () => {
       'The Benefits of Yoga'
     )
   })
+
+  test("if blogs initialised with no likes, it's set to 0", async () => {
+    const newBlog = {
+      "title": "The Benefits of Yoga",
+      "author": "Alexandra Kim",
+      "url": "https://www.benefitsofyoga.com",
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+
+    const blog = response.body
+
+    expect(blog.likes).toBeDefined()
+    expect(blog.likes).toBe(0)
+  })
+
+  test("if blogs initialised with no title, responds with 400", async () => {
+    const newBlog = {
+      "author": "Alexandra Kim",
+      "url": "https://www.benefitsofyoga.com",
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+
+  test("if blogs initialised with no url, responds with 400", async () => {
+    const newBlog = {
+      "title": "The Benefits of urls@",
+      "author": "Alexandra Kim",
+      "likes": 0
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
 })
+
+describe('deleting blogs is', () => {
+  test('successful if id exists', async () => {
+    const blogs = await helper.blogsInDb()
+    const id = blogs[0].id
+    await api.delete(`/api/blogs/${id}`).expect(204)
+  })
+
+  test('fails if id does not exist', async () => {
+    await api.delete("/api/blogs/ERRRO").expect(404)
+  })
+})
+
+describe('updating blogs', () => {
+  test('can update the likes for the blog', async () => {
+    const blogs = await helper.blogsInDb()
+    const firstBlog = blogs[0]
+    const id = firstBlog.id
+    const likes = firstBlog.likes
+
+    const alteredBlog = {
+      title: firstBlog.title,
+      url: firstBlog.url,
+      author: firstBlog.author,
+      likes: firstBlog.likes + 1
+    }
+    await api.put(`/api/blogs/${id}`).send(alteredBlog).expect(204)
+
+    const endBlogs = await helper.blogsInDb()
+    const blogWeUpdated = endBlogs.find((blog) => blog.id === id)
+
+    expect(blogWeUpdated.likes).toBe(likes + 1)
+  })
+
+})
+
+
 
 afterAll(async () => {
   await mongoose.connection.close()
