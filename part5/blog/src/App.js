55199f9abf758fabad13656ccd7d3d429988fd23
Daniel <vernond@tcd.ie>
2023-04-10 22:31:52 +0100

    5.17-5.22

--- part5/blog/src/App.js
+++ part5/blog/src/App.js
@@ -114,6 +114,7 @@ const App = () => {
       <div>
         username
         <input
+          id="username"
           type="text"
           value={username}
           name="Username"
@@ -123,13 +124,14 @@ const App = () => {
       <div>
         password
         <input
+          id="password"
           type="password"
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id="login-button" type="submit">login</button>
     </form>
   )
   const userInfoDisplay = () => (
@@ -145,7 +147,7 @@ const App = () => {
 
   const createBlogForm = () => (
     <Togglable buttonLabel='create blog' ref={blogFormRef}>
-      <BlogForm createNewBlog={createNewBlog}/>
+      <BlogForm createNewBlog={createNewBlog} blogFormRef={blogFormRef}/>
     </Togglable>
   )
 
