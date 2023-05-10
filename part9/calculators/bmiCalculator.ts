ee132e1e00509d90a0f045dc3a27dfa3c7dd7c6b
Daniel <vernond@tcd.ie>
2023-05-10 22:22:55 +0100

    part 9 ex9.1-9.3

--- part9/calculators/bmiCalculator.ts
+++ part9/calculators/bmiCalculator.ts
@@ -1,3 +1,9 @@
+interface HealthInformation {
+  height: number
+  weight: number
+}
+
+export {}
 
 const calculateBMI = (height: number, weight: number): string => {
   const bmi = weight / ((height / 100.0) ** 2);
@@ -13,4 +19,28 @@ const calculateBMI = (height: number, weight: number): string => {
   return "Obese"
 }
 
-console.log(calculateBMI(180, 74))
+const parseArguments = (args: string[]): HealthInformation => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBMI(height, weight));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage)
+}
--- part9/calculators/exerciseCalculator.ts
+++ part9/calculators/exerciseCalculator.ts
@@ -8,28 +8,36 @@ interface ExerciseResults {
   ratingExplanation: string
 }
 
-const calculateExercises = (exerciseHours: number[], targetHours: number) : ExerciseResults => {
-  const days: number = exerciseHours.length
-  const trainingDays: number = exerciseHours.filter((val) => val > 0).length
-  const averageTime: number = exerciseHours.reduce( (_, s) => _ + s, 0) / days 
+interface ExerciseInput {
+  exerciseHours: number[]
+  targetHours: number
+}
 
-  const totalTargetHours: number = targetHours * days
-  const targetReached: boolean = averageTime >= totalTargetHours
+export {}
+
+const calculateExercises = (exerciseHours: number[], targetHours: number) : ExerciseResults => {
+  console.log(exerciseHours)
+  const days: number = exerciseHours.length;
+  const trainingDays: number = exerciseHours.filter((val) => val > 0).length;
+  const averageTime: number = exerciseHours.reduce( (_, s) => _ + s, 0) / days;
+
+  const totalTargetHours: number = targetHours * days;
+  const targetReached: boolean = averageTime >= totalTargetHours;
   
   let rating: 1 | 2 | 3
   if (averageTime < 0.75 * totalTargetHours) {
-    rating = 1
+    rating = 1;
   } else if (averageTime < 1.25 * totalTargetHours) {
-    rating = 2
+    rating = 2;
   } else {
-    rating = 3
+    rating = 3;
   }
 
   let ratingExplanation: string
   switch (rating) {
-    case 1: ratingExplanation = 'lazybones'
-    case 2: ratingExplanation = 'not too bad but could be better'
-    case 3: ratingExplanation = 'wow'
+    case 1: ratingExplanation = 'lazybones'; break;
+    case 2: ratingExplanation = 'not too bad but could be better'; break;
+    case 3: ratingExplanation = 'wow'; break;
   }
 
   return {
@@ -40,7 +48,31 @@ const calculateExercises = (exerciseHours: number[], targetHours: number) : Exer
     targetReached,
     rating,
     ratingExplanation
+  };
+}
+
+const parseArguments = (args: string[]): ExerciseInput => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (isNaN(Number(args[2]))) throw new Error(`target hours must be a number, got '${args[2]}'`)
+  if (args.slice(3).some(hour => isNaN(Number(hour)))) throw new Error(`exercise hours must be numbers`)
+
+  const targetHours: number = Number(args[2]);
+  const exerciseHours: number[] = args.slice(3).map((hour) => Number(hour));
+  return {
+    targetHours,
+    exerciseHours
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+
+try {
+  const { exerciseHours, targetHours } = parseArguments(process.argv);
+  console.log(calculateExercises(exerciseHours, targetHours));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage)
+}
